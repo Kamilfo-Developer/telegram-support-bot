27e730b17eb1fbdf271b0f19958c154cd6de7785
Kamil Biktimirov <Biktimirov.kamil@yandex.ru>
2023-01-02 22:27:04 +0300

    BOT_TOKEN parameter added. Now it can be confugured using .env file

--- bot/settings.py
+++ bot/settings.py
@@ -1,11 +1,22 @@
 from typing import Type
 from bot.db.repositories.repository import Repo
 from bot.db.repositories.sa_repository import SARepo
+from dotenv import load_dotenv
+from pathlib import Path
+import os
+
+# Path to .env file where the bot token is stored
+DOTENV_PATH: Path | str = Path("../.env")
+
+load_dotenv(DOTENV_PATH)
+
 
 # Repository that will be used for getting access to the DB
-# Defaults to SARepo that implemented using SQLAlchemy
-repository: Type[Repo] = SARepo
+# Defaults to SARepo, which implemented using SQLAlchemy
+RepositoryClass: Type[Repo] = SARepo
 
 # Language code that satisfies IETF standard
 # (https://en.wikipedia.org/wiki/IETF_language_tag)
-default_language_code: str = "en"
+DEFAULT_LANGUAGE_CODE: str = "en"
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
