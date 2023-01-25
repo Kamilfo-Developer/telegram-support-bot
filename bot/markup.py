41cd60f27ff6b0bcdd6cbab1b25427704bd5826e
Kamil Biktimirov <Biktimirov.kamil@yandex.ru>
2023-01-25 23:00:27 +0300

    Added Markup class for getting keyboard markup for the bot

---
+++ bot/markup.py
@@ -0,0 +1,51 @@
+from bot.localization.messages import Messages
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+
+
+class Markup:
+    def __init__(self, messages: Messages):
+        self.messages = messages
+
+    def get_question_binding_buttons_markup(
+        self, bind_button_callback_data: str, unbind_button_callback_data: str
+    ) -> InlineKeyboardMarkup:
+        inline_keyboard = InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        self.messages.bind_question_button_text,
+                        callback_data=bind_button_callback_data,
+                    ),
+                    InlineKeyboardButton(
+                        self.messages.unbind_question_button_text,
+                        callback_data=unbind_button_callback_data,
+                    ),
+                ]
+            ]
+        )
+
+        return inline_keyboard
+
+    def get_answer_estimation_buttons_markup(
+        self,
+        useful_answer_button_callback_data: str,
+        unuseful_answer_button_callback_data: str,
+    ) -> InlineKeyboardMarkup:
+        inline_keyboard = InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        self.messages.estimate_answer_as_useful_button_text,
+                        callback_data=useful_answer_button_callback_data,
+                    ),
+                ],
+                [
+                    InlineKeyboardButton(
+                        self.messages.estimate_answer_as_unuseful_button_text,
+                        callback_data=unuseful_answer_button_callback_data,
+                    ),
+                ],
+            ]
+        )
+
+        return inline_keyboard
