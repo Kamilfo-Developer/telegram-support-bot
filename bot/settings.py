4f85fddf740d88912918ff2ddbfd99b7e9d79c32
Kamil Biktimirov <Biktimirov.kamil@yandex.ru>
2023-01-25 23:08:26 +0300

    Added new parameters

--- bot/settings.py
+++ bot/settings.py
@@ -5,18 +5,30 @@ from dotenv import load_dotenv
 from pathlib import Path
 import os
 
-# Path to .env file where the bot token is stored
-DOTENV_PATH: Path | str = Path("../.env")
+# Needed for some developing features
+# Should be False when production
+DEBUG = True
 
-load_dotenv(DOTENV_PATH)
+# Path to .env file where the bot token is stored
+DOTENV_PATH: Path | str = Path(".env")
+
+
+load_dotenv(dotenv_path=DOTENV_PATH)
 
 
 # Repository that will be used for getting access to the DB
-# Defaults to SARepo, which implemented using SQLAlchemy
+# Defaults to SARepo, which is implemented using SQLAlchemy
 RepositoryClass: Type[Repo] = SARepo
 
 # Language code that satisfies IETF standard
 # (https://en.wikipedia.org/wiki/IETF_language_tag)
-DEFAULT_LANGUAGE_CODE: str = "en"
+DEFAULT_LANGUAGE_CODE: str = "ru"
 
+# Your bot token, can be taken from here:
+# https://t.me/botfather
 BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+
+OWNER_ID = int(os.getenv("OWNER_ID"))
+
+OWNER_DEFAULT_DESCRIPTIVE_NAME = "Owner"
