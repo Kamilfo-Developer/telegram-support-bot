485fa0df94ade342af841846d18b6804d09901bf
Kamil Biktimirov <Biktimirov.kamil@yandex.ru>
2023-01-31 09:29:13 +0300

    Removed old DB initialization method

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,13 +1,3 @@
-from bot.init_db import init_sa_db
 from bot.bot import app
-from bot.db.db_sa_settings import engine, Base
-
-import asyncio
-
-loop = asyncio.new_event_loop()
-
-loop.run_until_complete(init_sa_db(engine, Base))
-
-loop.close()
 
 app.run_polling()
