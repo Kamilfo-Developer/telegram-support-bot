3455b3453e9a5c5337a1ec5544c1357a3760874c
Kamil Biktimirov <Biktimirov.kamil@yandex.ru>
2023-03-14 23:57:31 +0300

    Added dependency injection for get_messages function

--- bot/localization/get_messages.py
+++ bot/localization/get_messages.py
@@ -1,22 +1,28 @@
-from bot.settings import DEFAULT_LANGUAGE_CODE, TIMEZONE
 from bot.localization.messages import Messages
 from bot.localization.en_messages import ENMessages
 from bot.localization.ru_messages import RUMessages
+from datetime import timezone
+from pytz.tzinfo import DstTzInfo, BaseTzInfo, StaticTzInfo
+from typing import Type
 
 
-def get_messages(lang_code: str) -> Messages:
+def get_messages(
+    lang_code: str,
+    timezone: timezone | DstTzInfo | BaseTzInfo | StaticTzInfo,
+    default_lang_code: str = "en",
+) -> Messages:
     all_messages = {"ru": RUMessages, "en": ENMessages}
 
-    MessagesClass = all_messages.get(lang_code)
+    MessagesClass: Type[Messages] | None = all_messages.get(lang_code)
 
     if MessagesClass:
-        return MessagesClass(TIMEZONE)  # type: ignore
+        return MessagesClass(timezone)  # type: ignore
 
-    MessagesClass = all_messages.get(DEFAULT_LANGUAGE_CODE)
+    MessagesClass = all_messages.get(default_lang_code)
 
     if not MessagesClass:
         raise ValueError(
-            f"No messages for this DEFAULT_LANGUAGE_CODE: {DEFAULT_LANGUAGE_CODE}"
+            f"No messages for this DEFAULT_LANGUAGE_CODE: {default_lang_code}"
         )
 
-    return MessagesClass(TIMEZONE)  # type: ignore
+    return MessagesClass(timezone)
